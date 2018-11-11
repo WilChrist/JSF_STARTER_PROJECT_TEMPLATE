373be971805fd310a39f2f0bbd6a6f62371b2201
Willy Nzesseu <willynzesseu@gmail.com>
2018-11-12 00:19:02 +0100

    Basic Structure

---
+++ src/main/java/com/beautifullcard/internationalization/LanguageBean.java
@@ -0,0 +1,61 @@
+package com.beautifullcard.internationalization;
+
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
+import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
+
+@ManagedBean(name="language")
+@SessionScoped
+public class LanguageBean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String localeCode;
+
+    private static Map<String, Object> countries;
+
+    static {
+        countries = new LinkedHashMap<>();
+
+        countries.put("French", Locale.FRANCE);
+        countries.put("English", Locale.ENGLISH); //label, value
+    }
+
+    public Map<String, Object> getCountriesInMap() {
+        //System.out.println(countries.size());
+        return countries;
+    }
+
+
+    public String getLocaleCode() {
+        return localeCode;
+    }
+
+
+    public void setLocaleCode(String localeCode) {
+        this.localeCode = localeCode;
+    }
+
+    //value change event listener
+    public void countryLocaleCodeChanged(ValueChangeEvent e) {
+
+        String newLocaleValue = e.getNewValue().toString();
+
+        //loop country map to compare the locale code
+        for (Map.Entry<String, Object> entry : countries.entrySet()) {
+
+            if (entry.getValue().toString().equals(newLocaleValue)) {
+
+                FacesContext.getCurrentInstance()
+                        .getViewRoot().setLocale((Locale) entry.getValue());
+
+            }
+        }
+    }
+}
